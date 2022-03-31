265d2cf0f4a696f9d203bd8dea7409db99b08e79
Ido Strauss <strauss02@hotmail.co.il>
2022-03-31 17:15:06 +0300

    finish BIU development for now
    is in working state

--- src/ResultSheet.js
+++ src/ResultSheet.js
@@ -1,6 +1,7 @@
 class ResultSheet {
   constructor() {}
   results = {}
+  // TODO : add 'link' property that holds a link to the orignial page where results were extracted from.
 
   addResult(curriculum, departemnt, admissionStatus, enrollmentStatus) {
     this.results[curriculum] = {
