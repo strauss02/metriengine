85652beb21305be42ba66ce85af143dfcc2b70f9
Ido Strauss <strauss02@hotmail.co.il>
2022-04-03 12:41:50 +0300

    flip sides of object
    since the conversion is more comfortable when accessing value by the subject name as key

---
+++ src/TAU/IdBySubject.js
@@ -0,0 +1,49 @@
+export default {
+  civics: '063',
+  english: '010',
+  math: '014',
+  history: '020',
+  // hebrew for jewish sector
+  hebrew: '005',
+  //literature for jewsish sector
+  literature: '004',
+  //bible for jews
+  bible: '001',
+  'arabic for arabs': '094',
+  'hebrew for arabs': '089',
+  electronics: '050',
+  art: '027',
+  biology: '017',
+  geography: '041',
+  agriculture: '019',
+  electricity: '055',
+  chemistry: '016',
+  'social science': '022',
+  'computer science': '042',
+  music: '028',
+  // מחשבת ישראל
+  'jewish thought': '021',
+  //מכשור ובקרה
+  'control engineering': '053',
+  // מכניקה הנדסית
+  'engineering mechanics': '054',
+  // ערבית
+  arabic: '012',
+  physics: '015',
+  psychology: '073',
+  french: '011',
+  // שפה זרה אחרת
+  'foreign language': '013',
+  // תורה
+  torah: '003',
+  // תלמוד
+  talmud: '002',
+  //  אחר עם בונוס
+  'other with bonus': '096',
+  'other with bonus': '097',
+  'other with bonus': '000',
+  //  אחר ללא בונוס
+  'other without bonus': '080',
+  'other without bonus': '098',
+  'other without bonus': '099',
+}
